809d8448c70c54b6f6546156329a680cc5c51080
Felix Millne <felix.millne@btinternet.com>
2015-05-28 19:26:04 +0100

    Refactor for indirect updates and allow for changing crime title

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,12 +2,12 @@ package com.bignerdranch.android.criminalintent;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.UUID;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +19,7 @@ import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
+	public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
@@ -27,7 +28,10 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		mCrime =  new Crime();
+
+		UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
+		
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 	}
 	
 	@Override
@@ -35,6 +39,7 @@ public class CrimeFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 		
 		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
 		mTitleField.addTextChangedListener(new TextWatcher(){
 			public void onTextChanged(CharSequence c, int start, int before, int count){
 				mCrime.setTitle(c.toString());
@@ -61,6 +66,7 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setEnabled(false);
 		
 		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
 		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -69,5 +75,15 @@ public class CrimeFragment extends Fragment {
 		});
 		
 		return v;
+	}
+	
+	public static CrimeFragment newInstance(UUID crimeId){
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+		
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+		
+		return fragment;
 	}
 }
