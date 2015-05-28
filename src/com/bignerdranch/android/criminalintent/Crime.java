2a4c184480a9cd62b6b7b34e73cc0c022b0a5d6d
Felix Millne <felix.millne@btinternet.com>
2015-05-28 16:45:17 +0100

    Add date, checkbox and title to crime fragment

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,14 +1,18 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	
 	public Crime(){
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public String getmTitle() {
@@ -21,5 +25,21 @@ public class Crime {
 
 	public UUID getmId() {
 		return mId;
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date mDate) {
+		this.mDate = mDate;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean mSolved) {
+		this.mSolved = mSolved;
 	}
 }
